d327edf0407aa71ca93d7832195cfd6cbff617be
Ajaya Mati <ajaya.mati@nobroker.in>
2022-06-16 12:34:54 +0530

    changes

--- youtube-clone/ViewController.swift
+++ youtube-clone/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var j = 0
+        var j = 20
     }
 
 
